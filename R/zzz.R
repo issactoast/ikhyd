edc39f5a2d8e5427b0e5d4c29ea208339c8c2dde
Issac <issactoast@gmail.com>
2020-11-25 16:15:42 -0500

    citation attach to loading package

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+#' Cat citation message when the package is loading
+#'
+#' @name .onAttach
+#' @param libname
+#' @param pkgname
+#' @return start up message
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage(
+        paste("To cite this package use citation('ikhyd') \n\n",
+              "Text citation: \n\n",
+              crayon::bold("Issac Lee (2020)."),
+              crayon::italic("ikhyd"), ": I Know How You Drive - Collection of R functions to make telematics analysis easier.\n",
+              "R package version", crayon::red("0.1.0."),
+              crayon::blue("URL https://github.com/issactoast/ikhyd"))
+    )
+}
